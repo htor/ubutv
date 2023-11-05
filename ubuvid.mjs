6a1ea30f32e3d296371ec16b932b0592f16f0fc1
Herman Torjussen <herman.torjussen@gmail.com>
2023-11-05 22:12:31 +0100

    ubu pi

--- ubuvid.mjs
+++ ubuvid.mjs
@@ -1,43 +1,61 @@
 #!/usr/bin/env node
 
-import { exit } from 'node:process'
-import { execSync } from 'node:child_process'
+import { writeFileSync } from 'fs'
+import { exit } from 'process'
+import { spawnSync } from 'child_process'
+import { Readable } from 'stream'
 import { JSDOM } from 'jsdom'
+
+const cache = {}
+const url = 'https://www.ubu.com/film/index.html'
+const videoUrls = []
+let videoUrl
 
 function rand(val) {
   return Math.floor(Math.random() * val)
 }
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 async function crawl(href) {
-  console.log('CRAWLING:', href)
   const url = new URL(href)
-  if (!url.pathname.startsWith('/film')) throw new Error()
-  const res = await fetch(url.href)
-  const html = await res.text()
-  const dom = new JSDOM(html, { url: url.href })
-  const document = dom.window.document
+  if (!url.pathname.startsWith('/film')) return null
+  let document = cache[href]
+  if (!document) {
+    const res = await fetch(url.href)
+    const html = await res.text()
+    const dom = new JSDOM(html, { url: url.href })
+    cache[href] = document = dom.window.document
+  }
   const links = Array.from(document.querySelectorAll(':any-link'))
   const htmlLinks = links.filter(a => a.href.endsWith('.html') && a.pathname !== '/index.html')
   const videoLinks = links.filter(a => a.href.match(/.(m4a|mp4|mkv|m4v)$/g))
   if (videoLinks.length) return videoLinks[rand(videoLinks.length)].href
   if (htmlLinks.length) return crawl(htmlLinks[rand(htmlLinks.length)].href)
-  throw new Error()
+  return null
 }
 
-const url = 'https://www.ubu.com/film/index.html'
-let videoUrl
+async function main() {
+  while (videoUrls.length < 5) {
+    videoUrl = await crawl(url)
+    while (!videoUrl) {
+      await sleep(3000)
+      videoUrl = await crawl(url)
+    }
+    videoUrls.push(videoUrl)
+  }
+  writeFileSync('/tmp/ubu.m3u', videoUrls.join('\n'))
+  const cmd = spawnSync('vlc --random -f /tmp/ubu.m3u', { shell: true })
+  Readable.from(cmd.stdout).pipe(process.stdout)
+  Readable.from(cmd.stderr).pipe(process.stderr)
 
-try {
-  videoUrl = await crawl(url)
-  console.log('FOUND VIDEO:', videoUrl)
-} catch (_) {
-  console.log('NO VIDEO FOUND')
-  exit(1)
+  if (cmd.status !== 0) {
+    throw new Error(cmd.error ? cmd.error : '')
+  }
 }
 
-try {
-  execSync(`open -a /Applications/VLC.app ${videoUrl}`)
-} catch (e) {
-  console.log('FAILED TO OPEN VIDEO')
-  exit(1)
-}
+main()
