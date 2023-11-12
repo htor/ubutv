eb829b53d585188a24a1c0c9285295f548fa852d
Herman Torjussen <herman.torjussen@gmail.com>
2023-11-12 02:46:31 +0100

    make splash

--- ubutv.sh
+++ ubutv.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-$HOME/Programming/ubutv/ubutv.mjs | 
+echo -en 'WELCOME TO UBUTV!\n\nSEARCHING FOR VIDEOS...'
+
+while true 
+do 
+  echo -n .
+  sleep 10
+done &
+
+$HOME/Programming/ubutv/ubutv.mjs 2>&1 | 
   while read line 
-    do echo -e "$(date -Iseconds) $line" >> /tmp/ubutv.log
+  do 
+    echo -e "$(date -Iseconds) $line" >> /tmp/ubutv.log
   done
