cab79d775385f51b73aedde798f8ef02bc8eb062
Herman Torjussen <herman.torjussen@gmail.com>
2023-12-05 14:56:22 +0100

    shellcheck

--- ubutv.sh
+++ ubutv.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-dir=$(dirname $0)
+dir=$(dirname "$0")
 echo -e 'WELCOME TO UBUTV!\nSEARCHING FOR VIDEOS...'
 
-$dir/ubutv.mjs 2>&1 |
-  while read line
+"$dir"/ubutv.mjs 2>&1 |
+  while read -r line
   do
-    echo -e "$(date -Iseconds) $line" >> $dir/ubutv.log
+    echo -e "$(date -Iseconds) $line" >> "$dir"/ubutv.log
   done
